fbe00d4e1ab3159f32063fef6adb9205c76c5fe3
Shweta <arunbasappagudasi@Aruns-MacBook-Pro.local>
2020-07-24 16:44:03 -0700

    Created a listener for route /api/score-word

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request, jsonify, json
 from uuid import uuid4
 
 from boggle import BoggleWordList, BoggleBoard
@@ -24,7 +24,11 @@ def homepage():
     uuid = uuid4()
 
     board = BoggleBoard()
+    print (f"created board {board}")
+
     boards[uuid] = board
+    print (f"saving board{boards}")
+    print (f"one board in boardslist {boards[uuid]}")
 
     # store the uuid for the board in the session so that later requests can
     # find it
@@ -34,3 +38,33 @@ def homepage():
         "index.html",
         board=board)
 
+def is_word_in_wordlist(word):
+    """check if word is present in list of meaningful words"""
+    meaningful_wordlist = BoggleWordList()
+    return meaningful_wordlist.check_word(word)
+
+def is_word_on_board(word):
+    """check if word is present on board"""
+    unique_id = session[SESS_BOARD_UUID_KEY]
+    board_instance = boards.get(unique_id)
+    return board_instance.check_word_on_board(word)
+    
+@app.route('/api/score-word', methods=['POST'])
+def validateword():
+    """ validate words :
+        1: is word a meaningful word?
+        2: is word found on board?"""
+    
+    player_word = request.form.get('player_word')
+   
+    if is_word_in_wordlist(player_word) is False :
+        return jsonify(result = "Not-word") 
+
+    elif is_word_on_board(player_word) is False :
+        return jsonify(result = "Not-on-board")  
+        
+    else :
+        return jsonify(result =  "ok", word = player_word)
+        
+
+    
